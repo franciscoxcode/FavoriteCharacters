663d1bcc59b1efc6ef3e94a10d1dab5b7a6ca735
Francisco Casillas <chiccasillas@gmail.com>
2025-06-17 14:02:14 -0600

    feat: add CharacterCardView with pixel art characters and favorite toggle

--- FavoriteCharacters/ContentView.swift
+++ FavoriteCharacters/ContentView.swift
@@ -7,15 +7,67 @@
 
 import SwiftUI
 
+struct Character: Identifiable {
+    let id = UUID()
+    let name: String
+    let image: String
+    var favorite = false
+}
+
 struct ContentView: View {
+    @State var characters = [
+        Character(name: "Bart", image: "bart"),
+        Character(name: "Lisa", image: "lisa"), 
+        Character(name: "Homer", image: "homer"),
+        Character(name: "Marge", image: "marge"),
+        Character(name: "Maggie",  image: "maggie"),
+        Character(name: "Milhouse", image: "milhouse")
+        
+    ]
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        ZStack {
+            Color.yellow.opacity(0.1)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Choose your favorite character")
+                    LazyVGrid(columns: columns) {
+                        ForEach($characters){ $character in 
+                            CharacterCardView(character: $character)
+                        }
+                    }
+                    .padding()         
+            }
+        }
+    }
+}
+
+struct CharacterCardView: View {
+    @Binding var character: Character
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           
+            Button(action: {
+                character.favorite.toggle()
+            }){
+                VStack {
+                    Image(character.image)
+                        .resizable()
+                        .frame(width: 120, height: 120)
+                    Text(character.name)
+                    Image(systemName: character.favorite ? "heart.fill" : "heart")
+                }
+                
+            }
+            .foregroundColor(character.favorite ? .red : .black)
+            .padding(5)
+            
         }
-        .padding()
     }
 }
 
