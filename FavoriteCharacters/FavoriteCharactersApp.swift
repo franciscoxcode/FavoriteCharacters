17f54d995cd427dbdc65995746a338c12260a9c4
Francisco Casillas <chiccasillas@gmail.com>
2025-06-17 12:36:57 -0600

    Initial Commit

---
+++ FavoriteCharacters/FavoriteCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteCharactersApp.swift
+//  FavoriteCharacters
+//
+//  Created by Francisco Jean on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
